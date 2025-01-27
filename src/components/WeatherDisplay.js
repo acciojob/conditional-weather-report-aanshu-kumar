f76394c6a5c57cb402495bf4b96c1f78036c3402
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-27 13:14:25 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -5,7 +5,6 @@ const WeatherDisplay = ({temp,condi}) => {
     <div>
         <p style={{color:`${temp>20 ? "red" : "blue"}`}}>
         <span>Temperature: {temp}</span>
-        //<span>Conditions: {condi}</span>
         </p>
     </div>
   )
