f58879f88ab920ab10e2117cff3565e45884ee49
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-27 13:09:53 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const WeatherDisplay = ({temp,condi}) => {
   return (
-    <div >
-        <p style={{color:`${temp>20 ? "red" : "blue"}`}}><span>Temperature: {temp}</span></p>
-        <p style={{color:`${temp>20 ? "red" : "blue"}`}}><span>Conditions: {condi}</span></p>
-      
+    <div>
+        <p style={{color:`${temp>20 ? "red" : "blue"}`}}>
+        <span>Temperature: {temp}</span>
+        <span>Conditions: {condi}</span>
+        </p>
     </div>
   )
 }
