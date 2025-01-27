b534f83f25b1b666d4ebc333480321b03eef63a6
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-27 13:00:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+  const [temp, setTemp] = React.useState(30);
+  const [condi, setCondi] = React.useState("Sunny");
+
+useEffect(() => {
+  setTemp(25);
+  setCondi("Sunny");
+},[]);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay temp={temp} condi={condi} />
     </div>
   )
 }
